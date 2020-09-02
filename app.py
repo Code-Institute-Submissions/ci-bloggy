6341c4e3ee3dde986eb71faf3a782b79351253e4
ib-skoric <ib.skoric@outlook.com>
2020-09-02 21:59:26 +0100

    Congigure env and intialise Flask application

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, render_template
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1>Hello World</h1>"
+
+if __name__ == '__main__':
+  app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
