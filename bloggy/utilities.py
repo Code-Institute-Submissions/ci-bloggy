b3f84bd65c4dc5dcb2966642b146acafb5126374
ib-skoric <ib.skoric@outlook.com>
2020-09-09 22:23:40 +0100

    Add exising user, email and blog helpers + comments

--- bloggy/utilities.py
+++ bloggy/utilities.py
@@ -1,4 +1,16 @@
 from bloggy import mongo
 
-all_posts = mongo.db.posts.find()
-featured_posts = mongo.db.posts.find({"is_featured": True})
+# -------------------------------------- #
+#    Database filtering/query helpers    #
+# -------------------------------------- #
+all_posts = mongo.db.posts.find() # Fetch all posts from the database
+featured_posts = mongo.db.posts.find({"is_featured": True}) # Fetch featured posts from database
+
+def existing_user(existing_username):
+    return mongo.db.users.find_one({"username": existing_username.lower()})
+
+def existing_email(existing_email):
+    return mongo.db.users.find_one({"email": existing_email})
+
+def existing_blog(existing_blog_name):
+    return mongo.db.blogs.find_one({"blog_name": existing_blog_name})
