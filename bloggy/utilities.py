eddebc32a9c19bb01c68a37e1fcbb14ef34c935c
ib-skoric <ib.skoric@outlook.com>
2020-09-14 22:34:38 +0100

    Change all posts and featured posts to be a list to avoid issues with lazy loadin from MongoDB

--- bloggy/utilities.py
+++ bloggy/utilities.py
@@ -3,8 +3,8 @@ from bloggy import mongo
 # -------------------------------------- #
 #    Database filtering/query helpers    #
 # -------------------------------------- #
-all_posts = mongo.db.posts.find() # Fetch all posts from the database
-featured_posts = mongo.db.posts.find({"is_featured": True}) # Fetch featured posts from database
+all_posts = list(mongo.db.posts.find()) # Fetch all posts from the database
+featured_posts = list(mongo.db.posts.find({"is_featured": True})) # Fetch featured posts from database
 
 def check_username(username):
     return mongo.db.users.find_one({"username": username.lower()})
