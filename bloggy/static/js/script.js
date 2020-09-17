5f32e4ba91d79e50e21bbc0c1718f28ded83ca5f
ib-skoric <ib.skoric@outlook.com>
2020-09-17 20:49:06 +0100

    Move JS Scripts to separate file

---
+++ bloggy/static/js/script.js
@@ -0,0 +1,16 @@
+// Initialise summernote editor
+$('#summernote').summernote({
+    tabsize: 2,
+    height: 250,
+    toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['view', ['fullscreen', 'codeview', 'help']]
+    ]
+});
+
+
+
