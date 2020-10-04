a64afc9d833d5d69c077cb2b4299c795f75c5792
ib-skoric <ib.skoric@outlook.com>
2020-10-04 15:03:27 +0100

    Update sorting values for most recent to be correct

--- bloggy/profile/routes.py
+++ bloggy/profile/routes.py
@@ -26,9 +26,9 @@ def profile_page(username):
     get_profile_posts_pagination = get_users_posts(profile_id).skip((page-1) * per_page).limit(per_page)
     # Handle sorting if value is X sort by Y
     if request.form.get('sort') == "1":
-        profile_posts = get_profile_posts_pagination.sort("last_updated", -1)
-    if request.form.get('sort') == "2":
         profile_posts = get_profile_posts_pagination.sort("last_updated", 1)
+    if request.form.get('sort') == "2":
+        profile_posts = get_profile_posts_pagination.sort("last_updated", -1)
     if request.form.get('sort') == "3":
         profile_posts = get_profile_posts_pagination.sort("title", 1)
     if request.form.get('sort') == "4":
@@ -47,4 +47,4 @@ def profile_page(username):
     return render_template('profile.html', profile=profile,
                            profile_posts=profile_posts, profile_blog=profile_blog,
                            pagination=pagination, profile_username=profile_username,
-                           current_user=current_user)
+                           current_user=current_user, sorting_value=sorting_value)
